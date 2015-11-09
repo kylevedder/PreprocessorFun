4fec8cf2c83673c12ec2c8e01e76344d5c83e345
kylevedder <kvedder@umass.edu>
2015-11-09 11:10:24 -0500

    First commit

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define amaze(c, o, n, f, u, s, e, d)  o ## s ## e  ## c
+#define foo(d, r, e, g, s) amaze(n, e, r, o, t, a, i, l) 
+
+int foo(a, r, m, e, d) ()
+{
+  printf("Confused?\n");
+  return 0;
+}
